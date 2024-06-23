6a04c202e4bd6ca102b10bb07806c4e8eb3cdc77
Guythetwo <guythework12372@gmail.com>
2024-06-23 19:21:13 +0700

    Uploaded files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Hender/Hender";
+import Main from "./Main/Main";
+
+import './App.css'
+
+function App() {
+  return (
+    <body>
+      <Header />
+      <Main />
+    </body>
+  );
+}
+
+export default App;
---
+++ src/Main/Main.js
@@ -0,0 +1,212 @@
+import './Main.css'
+import png from '../Image/500x500.jpg'
+
+function Main() {
+    return(
+        <div className='mian'>
+            <ul>
+                <li>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+
+                <li className='margin'>
+                    <div>
+                        <img src={png} />
+                        <h3>NAME_TEST</h3>
+                        <p>ID_TEST</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Main;
